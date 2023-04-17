476afe09e130e0bba8be4596b4c0358d5b154c53
Lexey9105 <umpp120@gmail.com>
2023-04-17 16:15:31 +0300

    Сделано доп ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        test6();
+        test7();
+        test8();
+
+    }
+
+    public static void test6() {
+        int a = 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+        int result = a * (b + (c - d * e));
+        System.out.println(result * -1);
+    }
+
+    public static void test7() {
+        int a = 5;
+        int b = 7;
+        a = 5 * 7;
+        b = a / 7;
+        a = a / b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    public static void test8() {
+        int a = 749;
+        int b = a / 10;
+        b = b % 10;
+        System.out.println(b);
     }
 }
